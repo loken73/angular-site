08b6a5c74b406455bbee88a0a7a8f024d1a8ee67
Frank <lokenator73@gmail.com>
2018-06-19 13:01:21 -0400

    Created account component to register and log in accounts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { AppointmentComponent } from './appointment/appointment.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CalendarComponent,
-    AppointmentComponent
+    AppointmentComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
