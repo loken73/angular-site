4a2208304dacb7b6876f7ebf4e267d3e79424235
Frank <lokenator73@gmail.com>
2018-06-19 12:58:36 -0400

    Generated components for appointmnet calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CalendarComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule
