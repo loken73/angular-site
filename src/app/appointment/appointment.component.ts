756f3060aae979f18a8d5469ac4969c0b06bb439
Frank <lokenator73@gmail.com>
2018-06-19 18:47:36 -0400

    Added input, output, and event emitter in appointment modal ts

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-appointment',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentComponent implements OnInit {
 
+  @Input() public date: string;
+  @Output() dateNone = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  clicked(ev) {
+    if (ev.target.id === 'modal-background' || ev.target.id === 'submit') {
+      this.date = '';
+      this.dateNone.emit(this.date);
+    }
+  }
 }
